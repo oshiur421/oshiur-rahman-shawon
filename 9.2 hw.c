eee5571928c44e09d9a6f8bd66de85d0d84a562c
oshiur421 <oshiurrahmanshawon@gmail.com>
2024-10-26 23:40:52 +0600

    Add files via upload

---
+++ 9.2 hw.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int num, sum = 0;
+
+
+    printf("Enter a positive integer: ");
+    scanf("%d", &num);
+
+
+    while (num > 0) {
+        sum=sum+num%10;
+        num=num/ 10;
+    }
+
+    printf("Sum of digits = %d\n", sum);
+
+    return 0;
+}
+
